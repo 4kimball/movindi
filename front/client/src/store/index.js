e9bc075249451be142b546d6e4f2dc4868e11821
munimun <ansgml6491@naver.com>
2021-05-20 17:59:21 +0900

    0520 muni community ing

--- front/client/src/store/index.js
+++ front/client/src/store/index.js
@@ -6,7 +6,21 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     movies: [],
-    actors: []
+    actors: [],
+    reviews: [
+      {
+        title : '리뷰제목1',
+        content : '리뷰내용1',
+        user : '작성자1',
+        id : 1
+      },
+      {
+        title : '리뷰제목2',
+        content : '리뷰내용2',
+        user : '작성자2',
+        id : 2
+      }
+    ]
   },
   mutations: {
   },
---
+++ front/client/src/views/CommunityFree.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>자유게시판</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommunityFree',
+}
+</script>
+
+<style>
+
+</style>
