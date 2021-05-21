032137c9501e19e9fda5c4721b8747c2f684f7d4
kimmeh4 <mhko411@smail.kongju.ac.kr>
2021-05-21 17:44:33 +0900

    update main

--- front/client/src/views/Home.vue
+++ front/client/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <section>
-      <div class="today-movie container">
+    <section >
+      <div class="today-movie container ">
+        
         <div id="movie-info">
           <p>오늘의 영화</p>
           <h1>{{ movie.title }}</h1>
@@ -9,11 +10,41 @@
           <hr>
           <p>{{ movie.content }}</p>
         </div>
-      
         <img :src=movie.poster_path alt="">
-       
       </div>
-      <div class="recommand-movies"></div>
+      <div class="recommand-movies" id="bg-rain">
+        <div class="d-flex align-items-center h-100" id="movies">
+        <div class="buttons">
+          <button @click="handleClick" id="btn-clicked" class="btn-rain">#비도오고그래서</button>
+          <button @click="handleClick" class="btn-friend">#친구와 함께</button>
+          <button @click="handleClick" class="btn-family">#가족에 대해서</button>
+          <button @click="handleClick" class="btn-change">#기분전환</button>
+        </div>
+        <div class="images">
+          <img :src=movie.poster_path alt="" @click="detailMovie" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="0" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">{{ movie.title }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body d-flex">
+                    <div>  
+                     <p>{{ movie.content }}</p>
+                     </div>
+                    <img :src=movie.poster_path alt="" id="modal-img">
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          <img :src=movie.poster_path alt="">
+          <img :src=movie.poster_path alt="">
+          <img :src=movie.poster_path alt="">
+        </div>
+      </div>
+      </div>
       <div class="today-actor"></div>
       
       <div class="community"></div>
@@ -35,6 +66,23 @@ export default {
     
   },
   methods: {
+    handleClick(event) {
+      const beforeClicked = document.getElementById("btn-clicked")
+      const section = document.querySelector('.recommand-movies')
+      console.log(section)
+      beforeClicked.id = ""
+      const targetBtn = event.target
+      targetBtn.id = "btn-clicked"
+      if(targetBtn.className === 'btn-rain') {
+        section.id = 'bg-rain';
+      } else if(targetBtn.className ==='btn-friend') {
+        section.id = 'bg-friend';
+      } else if(targetBtn.className === 'btn-family') {
+        section.id = 'bg-family';
+      } else if(targetBtn.className === 'btn-change') {
+        section.id = 'bg-change';
+      }
+    },
     
   },
   computed: {
@@ -45,6 +93,7 @@ export default {
   created() {
     this.$store.dispatch('getMovies')
   },
+  
   
 
 }
@@ -92,11 +141,79 @@ export default {
   width: 100%;
   height: 1000px;
   background-color: navy;
+  
 }
 
+#bg-rain {
+  background-image: url('../assets/bg-rain.jpg');
+}
+
+#bg-friend {
+  background-image: url('../assets/bg-friend.jpg');
+  background-size: 500px;
+}
+
+#bg-family {
+  background-image: url('../assets/bg-family.jpg');
+  background-size: 700px;
+}
+
+#bg-change {
+  background-image: url('../assets/bg-change.jpg');
+
+}
 .home .community {
   width: 100%;
   height: 1000px;
   background-color: green;
 }
+
+.recommand-movies {
+  height: 100%;
+}
+
+.recommand-movies #movies {
+  margin-left: 5rem;
+  margin-right: 5rem;
+  align-items: center;
+}
+.recommand-movies .buttons {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 50%;
+  margin-right: 5rem;
+}
+.recommand-movies .buttons button {
+  border: 1px white solid;
+  color: white;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: rgba(255, 255, 255, 0);
+  font-weight: bold;
+}
+.recommand-movies .buttons button:hover {
+  background-color: rgba(255, 26, 117, 0.5);
+}
+.recommand-movies .images img {
+  margin-left: 2rem;
+  width: 300px;
+}
+
+#btn-clicked {
+  background-color: rgba(255, 26, 117, 0.8);
+  
+}
+
+
+.modal-header, .modal-body{
+  width: 100%;
+  background: linear-gradient(to bottom right, #262626, #595959);
+  opacity: 0.8;
+  color: white;
+  font-weight: bold;
+
+}
+
+
 </style>
