076e2148e94b338f9cc5e8dd01a098ffca85269f
kimmeh4 <mhko411@smail.kongju.ac.kr>
2021-05-22 17:48:40 +0900

    add user field - sns

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -5,5 +5,6 @@ from imagekit.processors import ResizeToFill
 
 class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    sns_id = models.CharField(max_length=100, default='')
+    sns_id = models.CharField(max_length=100, blank=False)
+    sns_type = models.CharField(max_length=100, blank=False)
     image = ProcessedImageField(upload_to='image/profile_image', processors=[ResizeToFill(480, 640)], format='JPEG', options={'quality':150}, blank=False )
